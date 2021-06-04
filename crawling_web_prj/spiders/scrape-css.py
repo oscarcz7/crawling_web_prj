f8148b71352c28826e0a4651f839561133c14a36
Oscar Catota <77303650+oscarcz7@users.noreply.github.com>
2021-06-04 11:52:24 -0500

    Second Commit
    Changes in spider crawling. Creation functions to write an excel and order the arrays

--- crawling_web_prj/spiders/scrape-css.py
+++ crawling_web_prj/spiders/scrape-css.py
@@ -1,6 +1,7 @@
 from os import execl
 import scrapy
 import pandas as pd
+import re
 
 class ToScrapeCSSSpider(scrapy.Spider):
     
@@ -17,7 +18,7 @@ class ToScrapeCSSSpider(scrapy.Spider):
         comments = []
         points = []
         #Tittle an order data extract
-        for register in (response.css("tr.athing") ):
+        for register in response.css("tr.athing"):
             order_number.append(register.css("td.title > span.rank::text").extract_first())
             title.append(register.css("td.title > a.storylink::text").extract_first()) 
             yield {
@@ -32,17 +33,39 @@ class ToScrapeCSSSpider(scrapy.Spider):
                 'comments': information.xpath('..//td[2]/a[3]/text()').extract_first(),
                 'points': information.css("span.score::text").extract_first()
             }
-        
-   
-        #Excel file creation for original data exporting
-        final_array = [order_number, title, comments, points]
-        df = pd.DataFrame(final_array).T
-        df.rename(columns= {'0':'order_number', '1':'title', '2':'comments', '3':'points'}, inplace=False)
-        df.to_excel(excel_writer="../crawling_web_prj/data_extracted.xlsx")
-        
-        print('imprimiendo')
+        final_comments = []
+        final_points = []
+        for com in comments:
+            if com and not com == "discuss":
+                for C_ in com.split():
+                    if C_.isdigit():
+                        final_comments.append(int(C_))
+            else:
+                final_comments.append(0)
+        for Po_ in points:
+            if Po_:
+                for P_ in Po_.split():
+                    if P_.isdigit():
+                        final_points.append(int(P_))
+            else:
+                final_points.append(0)   
+        ultimate_array = []
         for i in range(len(order_number)):
-            print(order_number[1])
-
-
-
+            dic = {"Order Number": order_number[i-1], "Title": title[i-1], "Comments": final_comments[i-1], "Points": final_points[i-1]}
+            ultimate_array.append(dic)
+        more_5_array = []
+        less_5_array = []
+        for X in ultimate_array:
+            if len(X.get("Title").split()) > 5:
+                more_5_array.append(X)
+            else:
+                less_5_array.append(X)
+        sorted_more_com = sorted(more_5_array, key=lambda k: k["Comments"])
+        sorted_more_fp = sorted(less_5_array, key=lambda k: k["Points"])
+        #Excel file creation 
+        df = pd.DataFrame(ultimate_array)
+        df2 = pd.DataFrame(sorted_more_com)
+        df3 = pd.DataFrame(sorted_more_fp)
+        df.to_excel("../crawling_web_prj/acumulative.xlsx", sheet_name='acumulative')
+        df2.to_excel("../crawling_web_prj/filter_comments.xlsx", sheet_name='filter_comments')
+        df3.to_excel("../crawling_web_prj/filter_points.xlsx", sheet_name='filter_points')
